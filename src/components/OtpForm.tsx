d2f3b941a4d7d064b012cf944398f212cb9fc902
Vishal Kumawat <vishal.kumawat@appinventiv.com>
2024-08-30 18:37:04 +0530

    otp Form v1

--- src/components/OtpForm.tsx
+++ src/components/OtpForm.tsx
@@ -4,15 +4,24 @@ import OtpInput from "react-otp-input";
 const OtpForm = () => {
   const [otp, setOtp] = useState("");
 
+  const handleSubmit = () => {
+    console.log("OTP : ", otp);
+  };
+
   return (
     <>
-      <div className="flex flex-col gap-4 items-center justify-center bg-white p-16 rounded-2xl min-w-[700px]">
+      <div className="flex flex-col gap-4 items-center justify-center bg-white p-20 rounded-2xl min-w-[700px]">
         <h1 className="font-bold text-5xl">Verify OTP</h1>
         <div className="flex gap-4 justify-center items-center">
           <h3 className="text-[#9748ff] text-2xl">Time Left</h3>
           <div className="bg-[#9748ff] px-4 py-2 text-xl rounded-lg text-white">
             00 : 00
           </div>
+        </div>
+        <div className="w-full text-left">
+          <p className="ml-3">
+            Enter the 6 digit OTP send to your mobile XXXXX78984
+          </p>
         </div>
         <div>
           <OtpInput
@@ -33,6 +42,17 @@ const OtpForm = () => {
               );
             }}
           />
+        </div>
+        <div className="w-full mt-6">
+          <button
+            onClick={handleSubmit}
+            className="bg-[#4a3ee6] text-white text-center w-full py-4 rounded-md"
+          >
+            Verify OTP
+          </button>
+        </div>
+        <div className="w-full text-center">
+          <p className="text-gray-600">Resend OTP</p>
         </div>
       </div>
     </>
